cb129f1089a4174fbca8e320b460b68269cc279c
Andre Attamimi <73837775+sionta@users.noreply.github.com>
2022-10-07 21:41:12 +0900

    Update alias.cmd

--- alias.cmd
+++ alias.cmd
@@ -7,7 +7,7 @@ doskey unalias=%~n0 /d $1
 
 if "%~1"=="" (
     echo Try '%~n0 /?' for more information.
-    echo:& doskey /macros | sort & exit /b
+    echo;& doskey /macros | sort & exit /b
 ) else if "%~1"=="/?" (
     goto :USAGE
 )
@@ -15,19 +15,19 @@ if "%~1"=="" (
 if not defined ALIASES (
     set "ALIASES=%USERPROFILE%\.aliases"
     if not exist "%USERPROFILE%\.aliases" (
-        echo:== Start with '= EQUALS' will not be executed.
-        echo:e.=explorer .
-        echo:np=notepad $1
-        echo:== Aliases similar like as on Unix.
-        echo:= source=call $*
-        echo:= which=where $1 2$Gnul
-        echo:= touch=type nul$G$1
-        echo:= grep=findstr $*
-        echo:= cat=type $*
-        echo:= ls=dir /d $*
-        echo:= ll=dir /x $*
-        echo:= cp=copy $*
-        echo:= mv=move $*
+        echo;== Starting with '= EQUALS' will not be executed.
+        echo;e.=explorer .
+        echo;np=notepad $1
+        echo;== Aliases similar like as on Unix.
+        echo;= source=call $*
+        echo;= which=where $1 2$Gnul
+        echo;= touch=type nul$G$1
+        echo;= grep=findstr $*
+        echo;= cat=type $*
+        echo;= ls=dir /d /o $*
+        echo;= ll=dir /o $*
+        echo;= cp=copy $*
+        echo;= mv=move $*
     )>"%USERPROFILE%\.aliases"
 )
 
@@ -71,19 +71,16 @@ if /i "%~1"=="/f" (
     endlocal & exit /b
 )
 
-@rem for /f "tokens=1,* delims==" %%a in ("%*") do (
-@rem     set alias=%%a
-@rem     set value=%%b
-@rem )
-@rem set tests=%alias: =%
-@rem if ["%tests%"] neq ["%alias%"] (
-@rem     echo Alias name '%alias%' cannot contains spaces
-@rem     endlocal
-@rem     exit /b
-@rem )
-
-set alias=%~1
-set value=%~2
+for /f "tokens=1,* delims==" %%a in ("%*") do (
+    set alias=%%a
+    set value=%%b
+)
+set tests=%alias: =%
+if ["%tests%"] neq ["%alias%"] (
+    echo %~n0: The alias name '%alias%' cannot contains spaces.
+    endlocal
+    exit /b
+)
 
 :FILTER
 findstr /vi /rc:"^%alias%.=*" "%ALIASES%">"%ALISES%.tmp"
@@ -94,49 +91,49 @@ type "%ALISES%.tmp">"%ALIASES%"
 findstr /vrc:"\<=\>" /c:"^=." "%ALIASES%">"%ALISES%.tmp"
 doskey /macrofile="%ALISES%.tmp"
 del /f /q "%ALISES%.tmp"
-if defined verbose echo: %verbose%
+if defined verbose echo; %verbose%
 endlocal & exit /b %errorlevel%
 
 :USAGE
-echo %~f0 aliases for %windir%\System32\doskey.exe
-echo:
+echo %~nx0 aka %windir%\system32\doskey.exe
+echo;
+echo   Define or display aliases.
+echo;
 echo USAGE: %~n0 [OPTIONS] [NAME[=DEFINITION]...]
-echo:
-echo   NAME          Name of the aliases
-echo   DEFINITION    Actual command of alias name.
-echo:
+echo;
+echo   NAME        Name of the aliases.
+echo   DEFINITION  Actual command of alias name.
+echo;
 echo OPTIONS:
-echo   /f [filename] Import aliases file default: %%USERPROFILE%%\.aliases
-echo   /d [NAME]     Remove alias name, can also use 'unalias [NAME]'
-echo   /reload       Reload aliases in current session, can also use 'realias'
-echo:
-echo The special characters that require double quotes:
-echo          ^<space^>
-echo          ^&^(^)^[^]^{^}^^=;!'+,`~
-echo:
-echo The special characters alias in definitions:
-echo   $B     Sends macro output to a command. Equivalent to using the pipe '^(' and '*'.
-echo   $T     Separates commands. Equivalent to '^&' allows multiple commands.
-echo   $L     Redirects input. Equivalent to the redirection symbol for input '^<'.
-echo   $G     Redirects output. Equivalent to the redirection symbol for output '^>'.
-echo   $1-$9  Batch parameters. Equivalent to the argument '%%1-%%9' in batch programs.
-echo   $*     Represents all the command-line. Equivalent to '%%*' in batch programs.
-echo   ^^%%     Signs %% in environment variables must be escaped in alias definition
-echo          surrounded by double quotes require only '^^%%'.
-echo:
-echo EXAMPLE:
-echo:
-echo   ^(1^) Signs %% in environment variables:
-echo       %~nx0 foo="notepad "^^%%aliases^^%%""
-echo:
-echo   ^(2^) Overwrite existing alias name with new definition:
-echo       %~nx0 foo="alias /d $1"
-echo:
-echo   ^(3^) Show contains alias name:
-echo       %~nx0 foo
-echo:
-echo   ^(4^) Remove alias name:
-echo       %~nx0 /d foo
+echo   /d [NAME]   Remove alias name, can also use 'unalias [NAME]'
+echo   /f [FILE]   Import alias file, default: '%%userprofile%%\.aliases'
+echo   /reload     Reload alias file to current session, also use 'realias'
+echo;
+@REM echo The special characters that require double quotes:
+@REM echo          ^<space^>^&^(^)^[^]^{^}^^=;!'+,`~
+@REM echo;
+echo The following are some special codes in alias definitions:
+echo   $B     Sends macro output to a command '^(' and '*'.
+echo   $T     Separates commands '^&'.
+echo   $L     Redirects input '^<'.
+echo   $G     Redirects output '^>'.
+echo   $1-$9  Batch parameters '%%1-%%9'.
+echo   $*     Represents all the command-line '%%*'.
+echo   ^^%%     Signs in environment variables '%%'.
+echo;
+echo EXAMPLES:
+echo;
+echo   %~nx0 foo=notepad.exe "^%%aliases^%%"
+echo       Signs %% in environment variables
+echo;
+echo   %~nx0 foo=%~nx0 /d $1
+echo       Overwrite existing alias name with new definitions
+echo;
+echo   %~nx0 fo
+echo       Display containing alias names
+echo;
+echo   %~nx0 /d foo
+echo       Remove the aliases name
 exit /b
 
 :: in command prompt
