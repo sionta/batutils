c6846f990684aa49605886c3a9d49ed7b4a243cf
Andre Attamimi <73837775+sionta@users.noreply.github.com>
2022-10-10 23:57:38 +0900

    Update arch.cmd

--- arch.cmd
+++ arch.cmd
@@ -2,31 +2,30 @@
 
 @echo off
 if "%~1" equ "/?" (
-    echo usage: %~n0 [/quiet - Return env-var OS_ARCH]
-    exit /B
-) else if "%~1"=="/quiet" (
+    echo usage: %~n0 [/q - Return env-var OS_XBIT]
+    exit /b
+) else if /i "%~1"=="/q" (
     set verbose=@rem
 ) else (
     set verbose=echo
 )
 
-if /I "%PROCESSOR_ARCHITECTURE%" EQU "X86" (
-if /I "%PROCESSOR_ARCHITEW6432%" EQU "AMD64" (
+if /i "%PROCESSOR_ARCHITECTURE%" EQU "X86" (
+if /i "%PROCESSOR_ARCHITEW6432%" EQU "AMD64" (
+    set OS_XBIT=x64
     %verbose% This is a 64-bit Architecture running in 32-bit mode
-    set OS_ARCH=x64
 ) else (
+    set OS_XBIT=x86
     %verbose% This is a 32-bit Architecture running in 32-bit mode
-    set OS_ARCH=x86
 )) else (
+    set OS_XBIT=x64
     %verbose% This is a 64-bit Architecture running in 64-bit mode
-    set OS_ARCH=x64
 )
 
-%verbose% OS_ARCH=%OS_ARCH%, PROCESSOR_ARCHITECTURE=%PROCESSOR_ARCHITECTURE%
-if "%verbose%"=="echo" set OS_ARCH=
+%verbose% %OS_XBIT%
+if "%verbose%"=="echo" set OS_XBIT=
 set verbose=
-exit /B
-
+exit /b
 
 @REM if /i "%PROCESSOR_ARCHITECTURE%"=="AMD64" set "OS_XBIT=x64"
 @REM if /i "%PROCESSOR_ARCHITECTURE%"=="ARM64" set "OS_XBIT=x86"
